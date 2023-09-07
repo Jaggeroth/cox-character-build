9c52e3f495091fadd41baf8d3357ef905cd9ab02
David S Rawlins <isdsr@bristol.ac.uk>
2023-09-07 23:52:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# cox-character-build
+1. Make sure you have Java installed. At least version 11.0.3. More recent versions like 17 work fine
+
+2. Build using: mvn package
+
+3. Edit the properties.cfg file with your Rebirth Username , password and target location for the HTML files
+   You may include the details for up to three accounts.
+
+4. Open a command prompt in that folder.
+
+5. Enter command: java -jar cox-character-build-0.0.1-jar-with-dependencies.jar
+
+6. Wait a minute or two while the programme lists out all your characters.
+
+7. Once it has ended, open the HTML folder and the index.html file
---
+++ src/main/java/coxCharacterBuild/CharacterProfile.java
@@ -0,0 +1,89 @@
+package coxCharacterBuild;
+
+public class CharacterProfile {
+	private String name;
+	private String architype;
+	private String alignment;
+	private String level;
+	private String origin;
+	private String primary;
+	private String secondary;
+	private String filename;
+	private String title;
+	public CharacterProfile(String name, String architype, String alignment, String level, String origin, String primary, String secondary, String filename, String title) {
+		this.name = name;
+		this.architype = architype;
+		this.alignment = alignment;
+		this.level = level;
+		this.origin = origin;
+		this.primary = primary;
+		this.secondary = secondary;
+		this.filename = filename;
+		this.title = title;
+	}
+	public CharacterProfile(String name, String architype, String alignment, int level, String origin, String primary, String secondary, String filename, String title) {
+		this.name = name;
+		this.architype = architype;
+		this.alignment = alignment;
+		this.level = String.valueOf(level);
+		this.origin = origin;
+		this.primary = primary;
+		this.secondary = secondary;
+		this.filename = filename;
+		this.title = title;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArchitype() {
+		return architype;
+	}
+	public void setArchitype(String architype) {
+		this.architype = architype;
+	}
+	public String getAlignment() {
+		return alignment;
+	}
+	public void setAlignment(String alignment) {
+		this.alignment = alignment;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getPrimary() {
+		return primary;
+	}
+	public void setPrimary(String primary) {
+		this.primary = primary;
+	}
+	public String getSecondary() {
+		return secondary;
+	}
+	public void setSecondary(String secondary) {
+		this.secondary = secondary;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
